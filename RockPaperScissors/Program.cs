e18c8d1ccf211bfc3945dc7e41fc82f1249ae0b0
MVPKenLin <mvpkenlin@live.com>
2025-02-24 22:57:11 +1100

    Updating from local

---
+++ RockPaperScissors/Program.cs
@@ -0,0 +1,60 @@
+﻿﻿using System;
+using static Move;
+
+int wins = 0;
+int draws = 0;
+int loses = 0;
+while (true)
+{
+	Console.Clear();
+	Console.WriteLine("Mini Game 01 - Rock, Paper, and Scissors!");
+	Console.WriteLine();
+
+	Console.WriteLine("Your next move, choose one of the following:");
+    Console.WriteLine("     [1]: rock");
+    Console.WriteLine("     [2]: paper");
+    Console.WriteLine("     [3]: scissors");
+    Console.WriteLine("     [9]: exit");
+    Console.WriteLine();
+GetPlayerMoveInput:
+	Move playerMove;
+	switch ((Console.ReadLine() ?? "").Trim().ToLower())
+	{
+		case "1": playerMove = Rock; break;
+		case "2": playerMove = Paper; break;
+		case "3": playerMove = Scissors; break;
+		case "9": Console.Clear(); return;
+		default: Console.WriteLine("Invalid Input. Valid input is 1, 2, 3, or 9. Please Try Again..."); goto GetPlayerMoveInput;
+	}
+    Console.WriteLine($"You chose {playerMove}."); 
+	Move computerMove = (Move)Random.Shared.Next(3);
+	Console.WriteLine($"The computer chose {computerMove}.");
+    Console.WriteLine();
+	switch (playerMove, computerMove)
+	{
+		case (Rock, Paper) or (Paper, Scissors) or (Scissors, Rock):
+			Console.WriteLine("You lose.");
+			loses++;
+			break;
+		case (Rock, Scissors) or (Paper, Rock) or (Scissors, Paper):
+			Console.WriteLine("You win.");
+			wins++;
+			break;
+		default:
+			Console.WriteLine("This game was a draw.");
+			draws++;
+			break;
+	}
+    Console.WriteLine($"=====================================");
+	Console.WriteLine($"Score: {wins} wins, {loses} loses, {draws} draws");
+    Console.WriteLine();
+	Console.WriteLine("Press Enter To Continue...");
+	Console.ReadLine();
+}
+
+enum Move
+{
+	Rock = 0,
+	Paper = 1,
+	Scissors = 2,
+}
---
+++ RockPaperScissors/README.md
@@ -0,0 +1,32 @@
+# MiniGame01
+<h1 align="center">
+	Rock, Paper, and Scissors
+</h1>
+
+<p align="center">
+	<a href="https://github.com/mvpkenlin/MiniGame01"><img src="/images/github-repo-black.svg" alt="GitHub repo"></a>
+	<a href="https://docs.microsoft.com/en-us/dotnet/csharp/"><img src="/images/language-csharp.svg" alt="Language C#"></a>
+	<a href="https://dotnet.microsoft.com/download"><img src="/images/dotnet-badge.svg" title="Target Framework" alt="Target Framework"></a>
+	<a href="/LICENSE"><img src="/images/license-MIT-green.svg" alt="License"></a>
+</p>
+
+<p> 
+This is a mini game created while learning the second week of the GitHub Copilot Learn and Cert program. </p>
+
+<p>
+
+## GameIntroduction
+
+Rock Paper Scissors is a guessing game. You and the computer will choose "Rock", "paper", or "scissors". Rock beats scissors. Paper beats rock. Scissors beats paper.
+
+## Input
+
+Commands used for this game:
+- `1` for `rock`
+- `2` for `paper`
+- `3` for `scissors`
+
+Commands to exit the game:
+- `9` for `exit`
+
+</p>
